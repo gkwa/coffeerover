35be3adbca00eeb51eca8cad9cbc3ff085c45d33
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-03 22:29:53 -0700

    Initial

---
+++ script0300.ps1
@@ -0,0 +1,8 @@
+New-Item -Type "directory" -Force -Path ~/Documents/PowerShell | Out-Null
+
+# powershell:
+New-Item -Type "directory" -Force -Path ~/Documents/WindowsPowershell | Out-Null
+@"
+Set-Location ~
+"@ | Out-File -Append -Encoding ASCII $profile
+. $profile
