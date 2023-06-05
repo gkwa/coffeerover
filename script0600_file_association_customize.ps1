6b5de656ecc0346bc99c142e61a3c90b8cb40456
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-04 17:37:40 -0700

    stuff

--- script0600_file_association_customize.ps1
+++ script0600_file_association_customize.ps1
@@ -1,34 +1,8 @@
-<#
-
-https://github.com/DanysysTeam/PS-SFTA
-
-Get-Help $script -full
-
-$assoc.GetEnumerator() | ForEach-Object {"$($_.Key) - $($_.Value)"}
-
-powershell loop over keys of hash
-
-$foods | ForEach-Object { $_ }
-#>
-
-$url = [System.Uri]'https://raw.githubusercontent.com/DanysysTeam/PS-SFTA/master/SFTA.ps1'
-$script = "sfta.ps1"
-
-If(!(Test-Path($script))){ Invoke-WebRequest -UseBasicParsing $url -OutFile $script }
-. ./$script
-
-$assoc = @{
-    '.py' = 'notepad.exe'
-    '.tmp' = 'notepad.exe'
-    '.conf' = 'notepad.exe'
-    '.log' = 'notepad.exe'
-    '.au3' = 'notepad.exe'
-    '.ps1' = 'notepad.exe'
-    '.vbs' = 'notepad.exe'
-    '.ini' = 'notepad.exe'
-}
-
-$assoc.GetEnumerator() | ForEach-Object {
-    Set-FTA $_.Value $_.Key
-    "$($_.Key) - $($_.Value)"
-}
+cmd /c assoc .py=txtfile
+cmd /c assoc .tmp=txtfile
+cmd /c assoc .conf=txtfile
+cmd /c assoc .log=txtfile
+cmd /c assoc .au3=txtfile
+cmd /c assoc .ps1=txtfile
+cmd /c assoc .vbs=txtfile
+cmd /c assoc .ini=txtfile
