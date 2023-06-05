aead58968bde43e8ef1cf8bbcfa0019ca000691d
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-05 14:30:37 -0700

    real men dont make git commit comments

--- golang-tools.ps1
+++ golang-tools.ps1
@@ -1,3 +1,5 @@
+$env:PATH += ";C:\Program Files\Git\bin"
+
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/ramya-rao-a/go-outline@latest
 go install github.com/tpng/gopkgs@latest
