8fe69d54e1298feda495cf6f2d4c3cbb7454e9e7
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-05 14:20:12 -0700

    real men dont make git commit comments

---
+++ golang-tools.ps1
@@ -0,0 +1,6 @@
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/ramya-rao-a/go-outline@latest
+go install github.com/tpng/gopkgs@latest
+go install golang.org/x/lint/golint@latest
+go install golang.org/x/tools/gopls@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
