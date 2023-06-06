3357addc48bb6dae78459bbba52072c77a83d3bd
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-06 08:39:49 -0700

    Fix error: All packages must be provided by the same module

--- golang-tools.ps1
+++ golang-tools.ps1
@@ -1,9 +1,8 @@
 $env:PATH += ";C:\Program Files\Go\bin"
 
-go install `
-  github.com/go-delve/delve/cmd/dlv@latest `
-  github.com/ramya-rao-a/go-outline@latest `
-  github.com/tpng/gopkgs@latest `
-  golang.org/x/lint/golint@latest `
-  golang.org/x/tools/gopls@latest `
-  honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/ramya-rao-a/go-outline@latest
+go install github.com/tpng/gopkgs@latest
+go install golang.org/x/lint/golint@latest
+go install golang.org/x/tools/gopls@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
