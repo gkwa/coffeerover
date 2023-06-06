f2d67e31f482914e6c05e4217c04162daa939407
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-06 08:52:03 -0700

    real men dont make git commit comments

--- script0900_shutdown_reboot_button.ps1
+++ script0900_shutdown_reboot_button.ps1
@@ -1,12 +1,24 @@
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1" -Force
 
+$scriptsPath = Join-Path -Path $env:USERPROFILE\Documents -ChildPath "Scripts"
+
+if (-not (Test-Path $scriptsPath)) {
+    New-Item -Path $scriptsPath -ItemType Directory | Out-Null
+}
+
+Install-ChocolateyShortcut `
+  -Arguments '-NoLogo' `
+  -WorkingDirectory "C:\WINDOWS\system32\WindowsPowerShell\v1.0" `
+  -TargetPath "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" `
+  -ShortcutFilePath "$scriptsPath\powershell -NoLogo.lnk" `
+
 Install-ChocolateyShortcut `
   -Arguments '-NoProfile -NoLogo -Command "shutdown -t 30 -s"' `
   -PinToTaskbar `
   -WorkingDirectory "C:\WINDOWS\system32\WindowsPowerShell\v1.0" `
   -TargetPath "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" `
   -IconLocation "$env:USERPROFILE\Documents\Scripts\17902_power_shutdown_icon.ico" `
-  -ShortcutFilePath "$env:USERPROFILE\\Documents\Scripts\shutdown_now.lnk" `
+  -ShortcutFilePath "$scriptsPath\shutdown_now.lnk" `
   -WindowStyle 0
 
 Install-ChocolateyShortcut `
@@ -15,5 +27,5 @@ Install-ChocolateyShortcut `
   -WorkingDirectory "C:\WINDOWS\system32\WindowsPowerShell\v1.0" `
   -TargetPath "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe" `
   -IconLocation "$env:USERPROFILE\Documents\Scripts\Dakirby309-Windows-8-Metro-Other-Power-Restart-Metro.256.ico" `
-  -ShortcutFilePath "$env:USERPROFILE\Documents\Scripts\reboot_now.lnk" `
+  -ShortcutFilePath "$scriptsPath\reboot_now.lnk" `
   -WindowStyle 0
